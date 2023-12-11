92eb661d4f471e8065652a5024372b25af66b303
AOKeklik <ao_keklik@hotmail.com>
2023-12-11 07:45:19 +0100

    Update --

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,70 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path")
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
 module.exports = {
-    mode: 'development',
-    entry: {
-      bundle: path.resolve(__dirname, 'src/js/index.js'),
-    },
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: '[name][contenthash].js',
-      clean: true,
-      assetModuleFilename: 'assets/[name][ext]',
-    },
-    module: {
-      rules: [
-        {
-            // HTML LOADER
-            test: /\.html$/,
-            loader: 'html-loader'
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        }
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Webpack App',
-        filename: 'index.html',
-        template: 'src/index.html',
-      })
-    ],
-  }
+	mode: "development",
+	entry: {
+		bundle: path.resolve(__dirname, "src/js/index.js"),
+	},
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "[name]-[contenthash].js",
+		clean: true,
+		assetModuleFilename: "assets/[name][ext]",
+	},
+	module: {
+		rules: [
+			{
+				// HTML LOADER
+				test: /\.html$/,
+				loader: "html-loader",
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {},
+					},
+					"css-loader",
+				],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name]-[contenthash].css",
+		}),
+		new HtmlWebpackPlugin({
+			title: "Webpack App",
+			filename: "index.html",
+			template: "src/index.html",
+		}),
+	],
+	optimization: {
+		// minimize: true,
+		minimizer: [new CssMinimizerPlugin(), new TerserWebpackPlugin()],
+	},
+}
